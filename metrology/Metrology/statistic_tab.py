3e97100f1522ea9b9573a519d0755faea104422d
MsKonfety <astafurovaann24@gmail.com>
2025-12-08 14:39:25 +0300

    Update statistic_tab.py

--- metrology/Metrology/statistic_tab.py
+++ metrology/Metrology/statistic_tab.py
@@ -1,13 +1,1 @@
-from PyQt5.QtWidgets import QVBoxLayout, QWidget, QLabel
 
-class StatisticTab(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setup_ui()
-    
-    def setup_ui(self):
-        """Настройка вкладки статистики"""
-        layout = QVBoxLayout()
-        self.setLayout(layout)
-        
-        
